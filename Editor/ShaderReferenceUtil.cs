3d1f852b4363dec800700334caf5fbca74698197
lihongwei <taecg@qq.com>
2022-03-16 15:45:18 +0800

    [资源]更新

--- Editor/ShaderReferenceUtil.cs
+++ Editor/ShaderReferenceUtil.cs
@@ -16,6 +16,55 @@ namespace taecg.tools.shaderReference
 {
     public class ShaderReferenceUtil
     {
+        //主按钮样式
+        private static GUIStyle style01;
+        private static GUIStyle Style01
+        {
+            get
+            {
+                if (style01 == null)
+                {
+                    style01 = new GUIStyle("label");
+                    style01.alignment = TextAnchor.MiddleLeft;
+                    style01.wordWrap = false;
+                    style01.fontStyle = FontStyle.Bold;
+                    style01.fontSize = ShaderReferenceEditorWindow.FONTSIZE;
+                }
+                return style01;
+            }
+        }
+
+        //说明样式
+        private static GUIStyle style02;
+        private static GUIStyle Style02
+        {
+            get
+            {
+                if (style02 == null)
+                {
+                    style02 = new GUIStyle("label");
+                    style02.wordWrap = true;
+                    style02.richText = true;
+                    style02.fontSize = ShaderReferenceEditorWindow.FONTSIZE - 4;
+                }
+                return style02;
+            }
+        }
+
+        //说明样式
+        private static GUIStyle style03;
+        private static GUIStyle Style03
+        {
+            get
+            {
+                if (style03 == null)
+                {
+                    style03 = new GUIStyle("box");
+                }
+                return style03;
+            }
+        }
+
         public static Dictionary<string, string> SearchDic = new Dictionary<string, string>();
 
         /// <summary>
@@ -25,22 +74,9 @@ namespace taecg.tools.shaderReference
         /// <param name="message">小标题内容</param>
         public static void DrawOneContent(string str, string message = null)
         {
-            //主按钮样式
-            GUIStyle style01 = new GUIStyle("label");
-            style01.alignment = TextAnchor.MiddleLeft;
-            style01.wordWrap = false;
-            style01.fontStyle = FontStyle.Bold;
-            style01.fontSize = ShaderReferenceEditorWindow.FONTSIZE;
-
-            //说明样式
-            GUIStyle style02 = new GUIStyle("label");
-            style02.wordWrap = true;
-            style02.richText = true;
-            style02.fontSize = ShaderReferenceEditorWindow.FONTSIZE - 4;
-
-            EditorGUILayout.BeginVertical(new GUIStyle("Box"));
-            EditorGUILayout.TextArea(str, style01);
-            EditorGUILayout.TextArea(message, style02);
+            EditorGUILayout.BeginVertical(Style03);
+            EditorGUILayout.TextArea(str, Style01);
+            EditorGUILayout.TextArea(message, Style02);
             EditorGUILayout.EndVertical();
 
             //添加到搜索字典中
