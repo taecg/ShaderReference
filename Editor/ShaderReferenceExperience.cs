7151cd6d646a260eb3aa35a22e957fa607d1a906
taecg <lihongwei@onemt.com.cn>
2024-05-14 14:48:39 +0800

    [资源]更新

--- Editor/ShaderReferenceExperience.cs
+++ Editor/ShaderReferenceExperience.cs
@@ -2,7 +2,7 @@
  * @file         ShaderReferenceExperience.cs
  * @author       taecg
  * @created      2024-03-22
- * @updated      2024-03-22
+ * @updated      2024-04-22
  *
  * @brief        平时积累的一些经验(大多是真机上出现的注意事项)
  */
@@ -27,6 +27,7 @@ namespace taecg.tools.shaderReference
         {
             scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
             ShaderReferenceUtil.DrawOneContent("分支", "尽量不要使用if或者switch去做大量的分支,否则在部分机型上会卡到怀疑人生!");
+            ShaderReferenceUtil.DrawOneContent("Stencil", "红米9C上开启深度图同时Shader中启用Stencil时会导致资源显示不可见,关闭任一个即可解决.");
             EditorGUILayout.EndScrollView();
         }
 
